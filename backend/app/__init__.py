99648204fa480f0f0ca5d5a9a414e09b0cc0daad
leynier <leynier41@gmail.com>
2019-02-12 10:12:43 -0500

    Implementar el container para el backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 from .config import config
+from .container import Container
+
+container = Container()
 
 
 def create_app(config_name):
@@ -8,6 +11,8 @@ def create_app(config_name):
     CORS(app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    container.init_app(app)
 
     from .controllers import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/')
