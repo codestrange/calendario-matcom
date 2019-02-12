1f8e7f6701b63dc51016b8279fa2ad6c791bf3e0
leynier <leynier41@gmail.com>
2019-02-12 10:19:19 -0500

    Inicializar el unitofwork

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 from .config import config
 from .container import Container
+from .database.unitofwork.sqlalchemy_unitofwork import UnitOfWorkSQLAlchemy
 
+unitofwork = UnitOfWorkSQLAlchemy()
 container = Container()
 
 
@@ -13,6 +15,11 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     container.init_app(app)
+    unitofwork.init_app(app)
+
+    repositories = []
+    for repository in repositories:
+        unitofwork.add_repository(repository)
 
     from .controllers import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/')
