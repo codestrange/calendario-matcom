d9951b9bc2693451f5e7afbb6d0cb6adb3b2554d
leynier <leynier41@gmail.com>
2019-03-01 20:58:10 -0500

    Insertar las entidades y repositorios

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,26 @@
 from os import getenv
 from app import create_app
+from app.database.entities import CourseEntity, EventEntity, GroupEntity, LocalEntity, \
+    NotificationEntity, OptionEntity, PermissionEntity, ResourceEntity, RoleEntity, TagEntity, \
+    UserEntity, VoteEntity
+from app.database.repositories.all import CourseRepository, EventRepository, GroupRepository, \
+    LocalRepository, NotificationRepository, OptionRepository, PermissionRepository, \
+    ResourceRepository, RoleRepository, TagRepository, UserRepository, VoteRepository
 
 app = create_app(getenv('FLASK_CONFIG') or 'default')
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=app.db, unitofwork=app.unitofwork,
+                CourseEntity=CourseEntity, EventEntity=EventEntity, GroupEntity=GroupEntity,
+                LocalEntity=LocalEntity, NotificationEntity=NotificationEntity,
+                OptionEntity=OptionEntity, PermissionEntity=PermissionEntity,
+                ResourceEntity=ResourceEntity, RoleEntity=RoleEntity, TagEntity=TagEntity,
+                UserEntity=UserEntity, VoteEntity=VoteEntity, CourseRepository=CourseRepository,
+                EventRepository=EventRepository, GroupRepository=GroupRepository,
+                LocalRepository=LocalRepository, NotificationRepository=NotificationRepository,
+                OptionRepository=OptionRepository, PermissionRepository=PermissionRepository,
+                ResourceRepository=ResourceRepository, RoleRepository=RoleRepository,
+                TagRepository=TagRepository, UserRepository=UserRepository,
+                VoteRepository=VoteRepository)
