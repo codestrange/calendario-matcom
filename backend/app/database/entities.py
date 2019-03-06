322c5571c0ad381308200bdde744a752ef667c70
leynier <leynier41@gmail.com>
2019-03-06 06:40:07 -0500

    Implementar los metodos para la autenticacion utilizando flask_httpauth

--- backend/app/database/entities.py
+++ backend/app/database/entities.py
@@ -1,7 +1,4 @@
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from werkzeug.security import check_password_hash, generate_password_hash
-from ..container import Container
-
+from werkzeug.security import generate_password_hash
 
 class CourseEntity:
     def __init__(self, name, year, freq, sem, id=None):
@@ -123,23 +120,6 @@ class UserEntity:
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def generate_auth_token(self, expires_in=3600):
-        s = Serializer(Container.instance().current_app.config['SECRET_KEY'], expires_in=expires_in)
-        return s.dumps({'id': self.id}).decode('utf-8')
-
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(Container.instance().current_app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except:
-            return None
-        repository = Container.instance().current_app.unitofwork.get_repository('UserRepository')
-        return repository.query().get(data['id'])
 
 
 class VoteEntity:
