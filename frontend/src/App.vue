646c8785470d97c141735b44f89bcf1328777cbb
cbermudez97 <cbermu.97@gmail.com>
2019-03-14 06:24:26 -0400

    Recolocar el css y js de las plantillas de bootstrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,9 +13,25 @@
         },
         methods: {
             
+        },
+        mounted() {
+            // let jqueryjs = document.createElement('script');
+            // jqueryjs.setAttribute('src', './assets/vendor/jquery/jquery.min');
+            // document.body.appendChild(jqueryjs);
+            // let bootstrapjs = document.createElement('script');
+            // bootstrapjs.setAttribute('src', './assets/vendor/bootstrap/js/bootstrap.bundle.min');
+            // document.body.appendChild(bootstrapjs);
+            // let jqueryeasing = document.createElement('script');
+            // jqueryeasing.setAttribute('src', './assets/vendor/jquery-easing/jquery.easing.min');
+            // document.body.appendChild(jqueryeasing);
+            // let sbadminjs = document.createElement('script');
+            // sbadminjs.setAttribute('src', './assets/js/sb-admin-2.min');
+            // document.body.appendChild(sbadminjs);
         }
     }
 </script>
 
 <style>
+    @import "assets/css/sb-admin-2.min.css";
+    @import "assets/vendor/fontawesome-free/css/all.min.css";
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
-import '../public/vendor/jquery/jquery.min';
-import '../public/vendor/bootstrap/js/bootstrap.bundle.min';
-import '../public/vendor/jquery-easing/jquery.easing.min';
-import '../public/js/sb-admin-2.min';
-import '../public/vendor/fontawesome-free/css/all.min.css';
-import '../public/css/sb-admin-2.min.css';
+import './assets/vendor/jquery/jquery.min';
+import './assets/vendor/bootstrap/js/bootstrap.bundle.min';
+import './assets/vendor/jquery-easing/jquery.easing.min';
+import './assets/js/sb-admin-2.min';
 
 Vue.config.productionTip = false;
 
