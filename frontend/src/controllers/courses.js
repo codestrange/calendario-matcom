d38bf689fcb62736f19aa35cfb32c7375944a495
cbermudez97 <cbermu.97@gmail.com>
2019-03-16 14:17:51 -0400

    Implementar el controlador de Recursos

---
+++ frontend/src/controllers/courses.js
@@ -0,0 +1,35 @@
+import Petitions from './petitions';
+import Endpoints from '../endpoints/endpoints';
+
+const data_key = 'calendario-matcom-courses';
+
+export default {
+    data: {
+        courses: []
+    },
+    saveMinData() {
+        localStorage.setItem(data_key, JSON.stringify(this.data.courses));
+    },
+    loadMinData() {
+        let stored = localStorage.getItem(data_key);
+        if (stored !== null) {
+            this.data.courses = JSON.parse(stored);
+        }
+    },
+    removeMinData() {
+        localStorage.removeItem(data_key);
+    },
+    getCourcesData(token) {
+        Resources.clearHeaders();
+        Resources.set_JSONHeaders(token, '');
+        return Petitions.get(Endpoints.courses_data).then(response => response.json(), response => console.log('Error getting the response.')).then(
+        json => {
+            if (json !== null && !json.hasOwnProperty('error')) {
+                this.data.courses = json;
+                this.saveMinData();
+                return true;
+            }
+            return false;
+        });
+    }
+}
