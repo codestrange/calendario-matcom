f0d460da43319626f098ca50976f45482ca48506
cbermudez97 <cbermu.97@gmail.com>
2019-03-16 17:52:33 -0400

    Implementar base de la vista para consultar el horario
    También se arreglaron algunos errores en los controladores de las
    entidades.

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -3,14 +3,14 @@
         <div class="row">
             <div class="col">
                 <div class="dropdown mb-4">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="asignaturas_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="asignaturas_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" @click="loadCourses">
                         Asignaturas
                     </button>
                     <div class="dropdown-menu animated--fade-in " aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
                         <div class="input-group m-2 " v-for="it in courses">
                             <div class="input-group-text bg-white">
-                                <input type="checkbox" aria-label="Checkbox for following text input">
-                                <span class="ml-2" id="basic-">{{it}}</span>
+                                <input type="checkbox" aria-label="Checkbox for following text input" @click="markItem(it)">
+                                <span class="ml-2" id="basic-">{{it.name}}</span>
                             </div>
                         </div>
                     </div>
@@ -18,14 +18,14 @@
             </div>
             <div class="col">
                 <div class="dropdown mb-4 ">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="resources_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="resources_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" @click="loadResources">
                         Recursos
                     </button>
                     <div class="dropdown-menu animated--fade-in " aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
                         <div class="input-group m-2 " v-for="it in resources">
                             <div class="input-group-text bg-white">
                                 <input type="checkbox" aria-label="Checkbox for following text input">
-                                <span class="ml-2" id="basi1-addon3">{{it}}</span>
+                                <span class="ml-2" id="basi1-addon3">{{it.name}}</span>
                             </div>
                         </div>
                     </div>
@@ -33,14 +33,14 @@
             </div>
             <div class="col">
                 <div class="dropdown mb-4 ">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="locales_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="locales_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" @click="loadLocals">
                         Locales
                     </button>
                     <div class="dropdown-menu animated--fade-in " aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
                         <div class="input-group m-2 " v-for="it in locals">
                             <div class="input-group-text bg-white">
                                 <input type="checkbox" aria-label="Checkbox for following text input">
-                                <span class="ml-2" id="basi3-addon3">{{it}}</span>
+                                <span class="ml-2" id="basi3-addon3">{{it.name}}</span>
                             </div>
                         </div>
                     </div>
@@ -48,14 +48,14 @@
             </div>
             <div class="col">
                 <div class="dropdown mb-4 ">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="tipos_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="tipos_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" @click="loadTags">
                         Tipos
                     </button>
                     <div class="dropdown-menu animated--fade-in " aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
-                        <div class="input-group m-2 " v-for="it in types">
+                        <div class="input-group m-2 " v-for="it in tags">
                             <div class="input-group-text bg-white">
                                 <input type="checkbox" aria-label="Checkbox for following text input">
-                                <span class="ml-2" id="basi5-addon3">{{it}}</span>
+                                <span class="ml-2" id="basi5-addon3">{{it.text}}</span>
                             </div>
                         </div>
                     </div>
@@ -63,14 +63,14 @@
             </div>
             <div class="col">
                 <div class="dropdown mb-4 ">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="grupos_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="grupos_drop_down" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" @click="loadGroups">
                         Grupos
                     </button>
                     <div class="dropdown-menu animated--fade-in " aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
                         <div class="input-group m-2 " v-for="it in groups">
                             <div class="input-group-text bg-white">
                                 <input type="checkbox" aria-label="Checkbox for following text input">
-                                <span class="ml-2" id="basi7-addon3">{{it}}</span>
+                                <span class="ml-2" id="basi7-addon3">{{it.name}}</span>
                             </div>
                         </div>
                     </div>
@@ -83,13 +83,83 @@
 <script>
     export default {
         name: "Home",
-        data(){
+        data () {
             return {
                 courses: ['DAA','Compilacion','BD2','IS'],
                 resources: ['Proyector1','Proyector2'],
                 locals: ['Aula1','Lab1'],
-                types: ['Cp','Conf','JC'],
+                tags: ['Cp','Conf','JC'],
                 groups: ['C312','C311','C411']
+            }
+        },
+        methods: {
+            loadCourses () {
+                let token = this.$store.state.user.getToken();
+                this.$store.state.courses.loadMinData();
+                if (this.$store.state.courses.data.courses.length !== 0) {
+                    this.courses = this.$store.state.courses.data.courses;
+                }
+                else {
+                    this.$store.state.courses.getCourcesData(token).then(result => {
+                        this.courses = this.$store.state.courses.data.courses;
+                    });
+                }
+            },
+            loadResources () {
+                let token = this.$store.state.user.getToken();
+                this.$store.state.resources.loadMinData();
+                if (this.$store.state.resources.data.resources.length !== 0) {
+                    this.resources = this.$store.state.resources.data.resources;
+                }
+                else {
+                    this.$store.state.resources.getResourcesData(token).then(result => {
+                        this.resources = this.$store.state.resources.data.resources;
+                    });
+                }
+            },
+            loadLocals () {
+                let token = this.$store.state.user.getToken();
+                this.$store.state.locals.loadMinData();
+                if (this.$store.state.locals.data.locals.length !== 0) {
+                    this.locals = this.$store.state.locals.data.locals;
+                }
+                else {
+                    this.$store.state.locals.getLocalsData(token).then(result => {
+                        this.locals = this.$store.state.locals.data.locals;
+                    });
+                }
+            },
+            loadTags () {
+                let token = this.$store.state.user.getToken();
+                this.$store.state.tags.loadMinData();
+                if (this.$store.state.tags.data.tags.length !== 0) {
+                    this.tags = this.$store.state.tags.data.tags;
+                }
+                else {
+                    this.$store.state.tags.getTagsData(token).then(result => {
+                        this.tags = this.$store.state.tags.data.tags;
+                    });
+                }
+            },
+            loadGroups () {
+                let token = this.$store.state.user.getToken();
+                this.$store.state.groups.loadMinData();
+                if (this.$store.state.groups.data.groups.length !== 0) {
+                    this.groups = this.$store.state.groups.data.groups;
+                }
+                else {
+                    this.$store.state.groups.getGroupsData(token).then(result => {
+                        this.groups = this.$store.state.groups.data.groups;
+                    });
+                }
+            },
+            markItem(item) {
+                if (item.hasOwnProperty('isMarked')) {
+                    item.isMarked = !item.isMarked;
+                }
+                else {
+                    item.isMarked = true;
+                }
             }
         }
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,23 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import UserController from './controllers/user';
 import RoutesController from './controllers/routes';
+import ResourcesController from './controllers/resources';
+import TagsController from './controllers/tags';
+import LocalsController from './controllers/locals';
+import GroupsController from './controllers/groups';
+import CoursesController from './controllers/courses';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
         user: UserController,
-        routes: RoutesController
+        routes: RoutesController,
+        resources: ResourcesController,
+        tags: TagsController,
+        locals: LocalsController,
+        groups: GroupsController,
+        courses: CoursesController
     },
     mutations: {},
     actions: {}
