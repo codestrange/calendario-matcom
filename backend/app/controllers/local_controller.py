69a6af8879b04e0091c2edcf53fbade115ac8d3e
Antonio <otano0398@gmail.com>
2019-03-23 17:06:21 -0400

    Virar cambios

--- backend/app/controllers/local_controller.py
+++ backend/app/controllers/local_controller.py
@@ -1,42 +1,17 @@
 from flask import jsonify, request
 from . import api
-from .event_controller import get_date
 from ..auth import auth_token
 from ..database import Local
-from ..utils import json_load
-
-
-def check_date1(event, json):
-    left = right = True
-    if 'start' in json and not 'end' in json:
-        left = right = event.end <= get_date(json.start)
-    elif 'end' in json and not 'start' in json:
-        left = right = event.start >= get_date(json.end)
-    elif 'start' in json and 'end' in json:
-        left = right = (event.end <= get_date(json.start) or event.start >= get_date(json.end) )             
-    return left and right
 
 
 @api.route('/locals')
 @auth_token.login_required
 def get_locals():
-    json = json_load(request.json)
-    _locals = Local.query.all()
-    free_locals = []
-    for local in _locals:
-        valid = True
-        events_at = local.events
-        for event in events_at:
-            if not check_date1(event, json):
-                valid = False
-                break
-        if valid:
-            free_locals.append(local)
+    locals = Local.query.all()
     return jsonify([{
         'id': local.id,
-        'local': local.name,
-        'size': local.size
-    } for local in free_locals])
+        'name': local.name
+    } for local in locals])
 
 
 @api.route('/locals/<int:id>')
