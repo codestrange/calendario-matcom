88d66dd7286f7682bd629829d2b5a3566a983002
leynier <leynier41@gmail.com>
2019-03-23 11:30:20 -0400

    Cambiar el visual del filtro y el order

--- frontend/src/components/Courses.vue
+++ frontend/src/components/Courses.vue
@@ -3,19 +3,25 @@
         <div class="row">
             <div class="col-12">
                 <div class="card mb-2 w-100 border-bottom-primary">
-                    <div class="card-header py-3 bg-white">
-                        <div class="row mb-3">
-                            <div class="col">
+                    <div class="card-header py-2 bg-white">
+                        <div class="row align-items-center">
+                            <div class="col-2">
                                 <h5 class="m-0 font-weight-bold text-primary">Asignaturas</h5>
                             </div>
-                        </div>
-                        <class class="row">
-                            <class class="col">
-                                <div class="input-group">
-                                    <input type="text" v-model="text" class="form-control bg-light border-0 small" placeholder="Inserte palabra para buscar..." aria-label="Search" aria-describedby="basic-addon2">
+                            <div class="col-9">
+                                <input type="text" v-model="text" class="form-control bg-light border-0 small" placeholder="Buscar ..." aria-label="Search" aria-describedby="basic-addon2">
+                            </div>
+                            <div class="col-1">
+                                <div class="row justify-content-end">
+                                    <button class="btn btn-primary btn-sm btn-circle ml-3">
+                                        <i class="fas fa-sort-alpha-down"></i>
+                                    </button>
+                                    <button class="btn btn-primary btn-sm btn-circle ml-3">
+                                        <i class="fas fa-sort-alpha-up"></i>
+                                    </button>
                                 </div>
-                            </class>
-                        </class>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="card">
