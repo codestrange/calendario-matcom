871e2609960e2c57fde0ff7653d59a73876f023b
leynier <leynier41@gmail.com>
2019-03-23 14:55:22 -0400

    Limpiar el calendar y dejarlo listo para implementar el editor

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,6 +1,16 @@
 <template>
     <div id="class_editor">
-        <full-calendar :events="events" :config="config" @event-selected="eventSelected"></full-calendar>
+        <div class="row justify-content-end">
+            <form class="form-inline">
+                <button class="btn ml-3" @click.prevent="prev">
+                    <i class="fas fa-arrow-left"></i>
+                </button>
+                <button class="btn ml-3" @click.prevent="next">
+                    <i class="fas fa-arrow-right"></i>
+                </button>
+            </form>
+        </div>
+        <full-calendar ref="calendar" :events="events" :config="config" @event-selected="eventSelected"></full-calendar>
     </div>
 </template>
 
@@ -26,13 +36,19 @@
                     locale: 'es',
                     editable: false,
                     selectable: false,
-                    navLinks: true,
+                    navLinks: false,
+                    weekNumbers: true,
+                    weekNumberTitle: 'S ',
+                    hiddenDays: [6, 0],
                     header: {
-                        left: 'prev,next',
+                        left: '',
                         center: 'title',
-                        right: 'agendaWeek,agendaDay'
+                        right: ''
                     },
-                    scrollTime: '08:00:00'
+                    scrollTime: '08:00:00',
+                    minTime: '08:00:00',
+                    maxTime: '18:30:00',
+                    allDaySlot: false
                 },
                 datetimeStart: '',
                 datetimeEnd: '',
@@ -41,6 +57,12 @@
         },
         methods: {
             eventSelected(event, jsEvent, view) {
+            },
+            next() {
+                this.$refs.calendar.fireMethod('next');
+            },
+            prev() {
+                this.$refs.calendar.fireMethod('prev');
             }
         },
         created() {
