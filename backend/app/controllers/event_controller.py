f8459abbefce7bd8fc671e4eb8277646babccde2
leynier <leynier41@gmail.com>
2019-03-24 18:53:43 -0400

    Implementar post para crear un evento chequeandose las colisiones

--- backend/app/controllers/event_controller.py
+++ backend/app/controllers/event_controller.py
@@ -2,7 +2,8 @@ from datetime import timedelta
 from flask import jsonify, request
 from . import api
 from ..auth import auth_token
-from ..database import Event, User
+from ..database import Event, User, Course, Group, Local, Resource, Tag, db
+from ..errors import bad_request
 from ..utils import check_json, query, merge, check_date, json_load
 
 
@@ -33,6 +34,64 @@ def query_events():
         'start': event.start,
         'end': event.end
     } for event in events if check_date(event, json)])
+
+
+@api.route('/events', methods=['POST'])
+@auth_token.login_required
+def post_event():
+    json = json_load(request.json)
+    check_json(json, ['title', 'description', 'start', 'end', 'courses', 'groups', 'locals',
+                      'resources', 'tags'])
+    if Event.query.filter_by(title=json.title).first():
+        return bad_request('Ya existe un evento con el mismo título.')
+    events = Event.query.filter(json.start <= Event.start).filter(Event.end <= json.end).all()
+    ok = True
+    for group_id in json.groups:
+        for event in events:
+            for group in event.groups:
+                if group.id == group_id:
+                    ok = False
+    for local_id in json.locals:
+        for event in events:
+            for local in event.locals:
+                if local.id == local_id:
+                    ok = False
+    for resource_id in json.resources:
+        for event in events:
+            for resource in event.resources:
+                if resource.id == resource_id:
+                    ok = False
+    if ok:
+        event = Event()
+        event.title = json.title
+        event.description = json.description
+        event.start = json.start
+        event.end = json.end
+        for identifier in json.courses:
+            item = Course.query.get_or_404(identifier)
+            event.courses.append(item)
+        for identifier in json.groups:
+            item = Group.query.get_or_404(identifier)
+            event.groups.append(item)
+        for identifier in json.locals:
+            item = Local.query.get_or_404(identifier)
+            event.locals.append(item)
+        for identifier in json.resources:
+            item = Resource.query.get_or_404(identifier)
+            event.resources.append(item)
+        for identifier in json.tags:
+            item = Tag.query.get_or_404(identifier)
+            event.tags.append(item)
+        db.session.add(event)
+        db.session.commit()
+        return jsonify({'message': 'Evento creado correctamente.'}), 201
+    return bad_request('No es posible crear el evento.')
+
+
+@api.route('/events', methods=['PUT'])
+@auth_token.login_required
+def put_event():
+    return jsonify({})
 
 
 @api.route('/events')
