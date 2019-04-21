ae9c9b5af902d1092077c788fa1cd19806681bf1
leynier <leynier41@gmail.com>
2019-04-21 17:16:13 -0400

    Implementación de los endpoints para las notificaciones

--- backend/app/controllers/__init__.py
+++ backend/app/controllers/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 api = Blueprint('api', __name__)
 
 from . import course_controller, event_controller, group_controller, interval_controller, \
-    local_controller, resource_controller, tag_controller, user_controller
+    local_controller, notification_controller, resource_controller, tag_controller, user_controller
---
+++ backend/app/controllers/notification_controller.py
@@ -0,0 +1,45 @@
+from flask import g, jsonify
+from . import api
+from ..auth import auth_token
+from ..database import db, Notification
+
+
+@api.route('/notifications')
+@auth_token.login_required
+def get_notifications():
+    user = g.current_user
+    notifications = user.notifications
+    result = []
+    mask = set()
+    for item in notifications:
+        noti = item.notification
+        group = item.group
+        if noti.id in mask:
+            _notif = next(filter(lambda x: x['id'] == noti.id, result))
+            if group.name != 'all':
+                _notif['groups'].append({'id': group.id, 'name': group.name})
+            _notif['seened'] = _notif['seened'] and item.seened
+            continue
+        mask.add(noti.id)
+        result.append({
+            'id': noti.id,
+            'title': noti.title,
+            'body': noti.body,
+            'date': noti.date,
+            'seened': item.seened,
+            'groups': [{'id': group.id, 'name': group.name}] if group.name != 'all' else []
+        })
+    return jsonify(result)
+
+
+@api.route('/notifications/seen/<int:id>', methods=['POST'])
+@auth_token.login_required
+def seen_notification(id):
+    user = g.current_user
+    noti = Notification.query.get_or_404(id)
+    for item in noti.notifications:
+        if item.user.id == user.id:
+            item.seened = True
+    db.session.add(noti)
+    db.session.commit()
+    return jsonify({'message': 'Notificación marcada como vista correctamente.'}), 201
